1ec99f2c9ddaadf84a48aa3a90783be5a377739a
Andrew Smith <a.smith@silentworks.co.uk>
2022-04-06 20:46:40 +0100

    Update config and add env example

--- examples/sveltekit/svelte.config.js
+++ examples/sveltekit/svelte.config.js
@@ -1,8 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import { loadEnv } from 'vite';
-
-process.env = { ...process.env, ...loadEnv(process.env.NODE_ENV, process.cwd()) };
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
